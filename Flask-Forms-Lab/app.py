1b3d925f87d7e534cddd05bc28fd75b46891b4d0
Kerenli24-meet <s216767889@igalalon.ort.org.il>
2023-07-19 18:28:16 +0300

    done

--- Flask-Forms-Lab/app.py
+++ Flask-Forms-Lab/app.py
@@ -8,15 +8,46 @@ app = Flask(  # Create a flask app
 )
 
 
-username = "siwarha"
+username = "Keren"
 password = "123"
-facebook_friends=["Loai","Kenda","Avigail", "George", "Fouad", "Gi"]
+facebook_friends=["emily","hila","alma", "noam", "eden", "lili"]
 
 
-@app.route('/')  # '/' for the default page
+@app.route('/', methods=['GET','POST'])  # '/' for the default page
 def login():
-  return render_template('login.html')
-  
+	if request.method == 'GET':
+		return render_template('login.html')
+	else:
+		user = request.form['username']
+		pas = request.form['password']
+		if  user== username and pas==password:
+			return render_template('home.html', u=username, p=password, list=facebook_friends)
+		else:
+			return render_template('login.html')
+
+
+
+
+@app.route('/home')  # '/' for the default page
+def home():
+		return render_template('home.html')
+
+
+
+@app.route('/friend_exists/<string:name>')  # '/' for the default page
+def friendex(name):
+	name_in =""
+	if name in facebook_friends:
+		name_in = "true"
+		return render_template('friend_exists.html', e = name_in)
+
+	else:
+		name_in = "false"
+		return render_template('friend_exists.html', e = name_in)
+
+
+
+
 
 
 
